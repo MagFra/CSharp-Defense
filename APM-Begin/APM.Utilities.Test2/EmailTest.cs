66e663f9decd6ffd6841b5cae843e2ab2d4a8f6d
MagFra <47413425+magfra@users.noreply.github.com>
2023-09-21 17:51:33 +0200

    Försöker att pusha till mitt git

---
+++ APM-Begin/APM.Utilities.Test2/EmailTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APM.SL.Test
+{
+    public class EmailTest
+    {
+        [Fact]
+        public void SendEmail_WhenValidValues_ShouldReturnTrue()
+        {
+            // Arrange
+            var expected = true;
+
+            // Act
+            bool actual = Utility.SendEmail("Jack Harkness", "Today's Meeting",
+                                          "Please confirm our 1PM meeting",
+                                          DateTime.Now);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SendEmail_WhenOptionalValues_ShouldReturnTrue()
+        {
+            // Arrange
+            var expected = true;
+
+            // Act
+            bool actual = Utility.SendEmail("Jack Harkness", "Today's Meeting",
+                                          "Please confirm our 1PM meeting",
+                                          DateTime.Now,
+                                          saveCopy: true, highPriority: true,
+                                          includeSignature: false);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
